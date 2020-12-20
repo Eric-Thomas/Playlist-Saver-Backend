625099d8f0dcb04408365159a4360f4134f24c95
Jack Dahms <jack.dahms@gmail.com>
2020-12-20 01:12:41 -0600

    Implemeted Serializable

--- src/main/java/com/psb/model/repository/Album.java
+++ src/main/java/com/psb/model/repository/Album.java
@@ -1,9 +1,16 @@
 package com.psb.model.repository;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class Album {
+public class Album implements Serializable {
 
+	/**
+	 * Generated serial version ID
+	 */
+	private static final long serialVersionUID = -6345748435010321882L;
+	
 	private String name;
 }
--- src/main/java/com/psb/model/repository/Playlist.java
+++ src/main/java/com/psb/model/repository/Playlist.java
@@ -1,12 +1,17 @@
 package com.psb.model.repository;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class Playlist {
+public class Playlist implements Serializable {
 	
+	/**
+	 * Generated serial version ID
+	 */
+	private static final long serialVersionUID = 1680150966024931641L;
 	private String playlistName;
 	private List<Track> tracks;
 

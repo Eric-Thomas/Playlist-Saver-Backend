4cd34e273e3017ebb841600d838d37d07404acd1
Jack Dahms <jack.dahms@gmail.com>
2020-12-30 13:31:33 -0600

    tried to tell it to follow redirects

--- src/main/java/com/psb/config/SpringConfig.java
+++ src/main/java/com/psb/config/SpringConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class SpringConfig {
@@ -16,13 +19,16 @@ public class SpringConfig {
 	
 	@Bean
 	public WebClient getWebClientBuilder(){
-	    return   WebClient.builder().exchangeStrategies(ExchangeStrategies.builder()
-	            .codecs(configurer -> configurer
-	                      .defaultCodecs()
-	                      .maxInMemorySize(16 * 1024 * 1024))
-	                    .build())
+	    return   WebClient.builder()
+	    		.exchangeStrategies(ExchangeStrategies.builder()
+	    			.codecs(configurer -> configurer
+	    		        .defaultCodecs()
+	                    .maxInMemorySize(16 * 1024 * 1024))
+	                .build())
 	    		.baseUrl(spotifyBaseUrl)
 				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-	                  .build();
+				.clientConnector(new ReactorClientHttpConnector(
+						HttpClient.create().followRedirect(true)))
+	            .build();
 	}
 }

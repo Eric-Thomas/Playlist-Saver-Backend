7b7bd2f1fc43cc718977e224f901c392e0163766
Eric Thomas <ethomasosu@gmail.com>
2021-07-01 18:16:30 -0400

    change paths to load all users and playlist based on id

--- src/main/java/com/psb/controller/S3Controller.java
+++ src/main/java/com/psb/controller/S3Controller.java
@@ -3,21 +3,14 @@ package com.psb.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.psb.client.AWSS3Client;
 import com.psb.exception.AWSS3ClientException;
 import com.psb.exception.AWSS3ClientNotFoundException;
-import com.psb.model.spotify.SpotifyPlaylist;
-import com.psb.model.spotify.SpotifyPlaylists;
-import com.psb.util.Compresser;
-
-import software.amazon.awssdk.core.ResponseBytes;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 @RestController
 @RequestMapping("/s3")
@@ -29,17 +22,16 @@ public class S3Controller {
 	public S3Controller(AWSS3Client s3Client) {
 		this.s3Client = s3Client;
 	}
-
-	@SuppressWarnings("unchecked")
-	@GetMapping(path = "/load")
-	public SpotifyPlaylists load(@RequestParam String id) throws AWSS3ClientException, AWSS3ClientNotFoundException {
-		SpotifyPlaylists playlists = new SpotifyPlaylists();
-		// Spotify usernames are unique, so we'll use those to identify bucket objects
-		String objectKey = id;
-		ResponseBytes<GetObjectResponse> objectBytes = s3Client.getData(objectKey);
-		Object object = SerializationUtils.deserialize(Compresser.decompress(objectBytes.asByteArray()));
-		playlists.setPlaylists((List<SpotifyPlaylist>) object);
-		return playlists;
+	
+	@GetMapping(path = "/load/users/{id}/playlists")
+	public Object loadPlaylists(@PathVariable String id) throws AWSS3ClientException, AWSS3ClientNotFoundException {
+		return s3Client.getPlaylists(id);
+		
+	}
+	
+	@GetMapping(path = "/load/users")
+	public List<String> loadUsers() throws AWSS3ClientException {
+		return s3Client.getAllUsers();
 	}
 
 }
